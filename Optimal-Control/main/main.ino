d335c99413c088812dad3f7ff963fc29eed8f0c8
philam2001 <lamyenphi14@gmail.com>
2023-03-17 04:07:55 -0700

    Finish base code with PID and encoder pulses parsing

--- Optimal-Control/main/main.ino
+++ Optimal-Control/main/main.ino
@@ -1,9 +1,16 @@
+#include <PID_v1.h>
 #include <HCSR04.h>
 
 #define PLDCOMM 10
+#define MAXPULSE 16383 //14-bit
+#define MOTORFREQ 30000
+#define LEDFREQ 1
+
 #define MOTORPIN 10
 #define IN1PIN 11
 #define IN2PIN 12
+#define IN1CHANNEL 1
+#define IN2CHANNEL 1
 #define LEDPIN 32
 #define LEDCHANNEL 0
 #define SENSOR2PIN 33
@@ -16,10 +23,17 @@
 #define SEVENTYPERC 45876
 
 /* global variable declaration */
-int pulse = 0b0;
+double setPosition = 8544;
+double input, output;
+double Kp = 0.5, Ki = 0.1, Kd = 0.5;
+int pulse = 0;
+int pulseOverflow = 0;
 bool home = true;
+bool flag = false;
+
 hw_timer_t* pidTimer = NULL;
 UltraSonicDistanceSensor distanceSensor(3, 4); //Initialize sensor
+PID motorPID(&input, &output, &setPosition, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
@@ -33,15 +47,22 @@ void setup()
 
   //LED states at 1Hz PWM and 16 bit resolution at channel 0
   ledcAttachPin(LEDPIN, LEDCHANNEL);
-  ledcSetup(LEDCHANNEL, 1, 16);
+  ledcSetup(LEDCHANNEL, LEDFREQ, 16);
+
+  //Motor PWM setup 30kHz at channel 1 and 2
+  ledcAttachPin(IN1PIN, IN1CHANNEL);
+  ledcSetup(IN1CHANNEL, MOTORFREQ, 8);
+
+  ledcAttachPin(IN2PIN, IN2CHANNEL);
+  ledcSetup(IN2CHANNEL, MOTORFREQ, 8);
 
   //ISRs
-  //This lets timer1 frequency 80MHz/80kHz=1Khz
-  hw_timer_t* pidTimer = timerBegin(2, 80000, true);
+  //This lets timer1 frequency = 80MHz
+  hw_timer_t* pidTimer = timerBegin(2, 1, true);
   timerAttachInterrupt(pidTimer, &PID, true);
-  //Every 500ms PID will run
-  timerAlarmWrite(pidTimer, 500 ,true);
-  timerAlarmEnable(pidTimer);
+  //Every 1Khz PID will run
+  timerAlarmWrite(pidTimer, 80000 ,true);
+  motorPID.SetMode(AUTOMATIC);
 
   attachInterrupt(digitalPinToInterrupt(SENSOR2PIN), openClaw, RISING);
 }
@@ -49,6 +70,7 @@ void setup()
 void loop()
 {
   ledcWrite(LEDCHANNEL, SEVENTYPERC);
+  digitalWrite(MOTORPIN, LOW);
   float distance = distanceSensor.measureDistanceCm();
   
   if (distance <= 15.0 && distance >= 0&& home) {
@@ -59,30 +81,64 @@ void loop()
 void closeClaw()
 {
   ledcWrite(LEDCHANNEL, TENPERC);
-
-  // PIN D10-D13
+  setPosition = 8544;
+  flag = true;
+  
   digitalWrite(MOTORPIN, HIGH);
+  ledcWrite(IN2PIN, 0);
 
-  // Motor spin forward
-  digitalWrite(IN1PIN, HIGH);
-  digitalWrite(IN2PIN, LOW);
-
+  // Start PID
   home = false;
+  timerAlarmEnable(pidTimer);
+
+  // Should not get to this case, but goes to next state if PID > 30s
+  delay(30000);
 
   // Next state: Travel Claw
   travelClaw();
 }
 
 void PID() {
-  if !home {
-    int pulsetemp = 0b0;
+  if (!home) {
+    int pulseTemp = 0b0;
+    bool pin;
 
     for (int i = 0; i < PLDCOMM; i++)
     {
       // PIN D0 - D9 (D0-D9)
-      pulsetemp = pulsetemp << 1 | int(digitalRead(i));
+      pulseTemp = pulseTemp << 1 | int(digitalRead(i));
     }
-    // Calculate error, return difference 
+
+    //Motor running backwards
+    // case that pulse count overflows backwards from 200 -> 15950
+    // or motor goes backward 800 -> 400, chose 1000 as buffer as
+    // pulses should not be greater than that with PID ISR
+    if ((pulseTemp)*0.5 > pulseOverflow || (pulseOverflow-pulseTemp < 1000 && pulseOverflow-pulseTemp > 0)){
+      pulse = pulse-(MAXPULSE + pulseOverflow - pulseTemp);
+      pin = IN2PIN;
+    } else {
+    //Motor running forwards
+    // case if pulse is regularly larger or is less than prev pulse
+      pulse = pulse+(pulseTemp-pulseOverflow);
+      pin = IN1PIN;
+    }
+
+    pulseOverflow = pulseTemp;
+    input = pulse;
+    motorPID.Compute();
+
+    ledcWrite(pin, 255*abs((setPosition-output)/setPosition));
+    
+    // PID settles close enough
+    if (output <= setPosition+1 && output >= setPosition-1) {
+      timerAlarmDisable(pidTimer);
+      
+       if (flag){
+        travelClaw();
+      } else {
+        openClaw();
+      }
+    }
   }
 }
 
@@ -91,9 +147,16 @@ void travelClaw()
   home = true;
   ledcWrite(LEDCHANNEL, FORTYPERC);
   
-  // Motor stop spin
-  digitalWrite(IN1PIN, LOW);
-  digitalWrite(IN2PIN, LOW);
+  // Motor stop spin, sanity check
+  ledcWrite(IN1PIN, 0);
+  ledcWrite(IN2PIN, 0);
+
+  // Should not get to this case, but goes to next state if 
+  // it doesn't see sensor for > 30s
+  delay(30000);
+
+  // Next state: openClaw()
+  openClaw();
 }
 
 // Last state (ISR): 
@@ -101,18 +164,18 @@ void openClaw()
 {
   // ISR sense
   ledcWrite(LEDCHANNEL, ONEHUNPERC);
+  setPosition = 0;
+  flag = false;
 
-  // PIN D10-D13
-  digitalWrite(MOTORPIN, HIGH);
+  ledcWrite(IN1PIN, 0);
 
-  home == false;
+  // Start PID
+  home = false;
+  timerAlarmEnable(pidTimer);
 
-  // Motor spin forward
-  // Do this until a certain point
-  digitalWrite(IN1PIN, LOW);
-  digitalWrite(IN2PIN, HIGH);
- 
-  // End
-  digitalWrite(IN1PIN, LOW);
-  digitalWrite(IN2PIN, LOW);
+  // Should not get to this case, but goes to next state if PID > 30s
+  delay(30000);
+
+  // Next state: Home
+  home = true;
 }
